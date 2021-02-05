d447327d94be46214cb702d07acbe54e48b9c4cf
Jay Steigner <ioctosteigner@gmail.com>
2021-02-04 22:06:58 -0600

    (add) added data checker statements to user property validation function

--- userCreation.js
+++ userCreation.js
@@ -4,16 +4,21 @@ const userCreation = (name, age, height) => {
     return "please include name, age, height.";
   }
 
-  return dbInsertQuery({ name, age: +age, height });
+  return dbInsertQuery({ name, age, height });
 };
 
 const userPropertyValidation = (name, age, height) => {
-  if (!name && !age && !height) {
+  if (!name || !age || !height) {
     return false;
   }
+  // ask someone about best practices about this
+    // Is this function doing too much or should it be extrapolated out
+  if (typeof name !== 'string') return false;
+  if (typeof height !== 'string') return false;
+
 };
 
-const dbInsertQuery = ({ age, height, name}) => {
+const dbInsertQuery = ({ age, height, name }) => {
   const newUser = {age, height, name };
   db.query(`INSERT INTO users (age, height, username) VALUES ('${age}','${height}', '${name}')`);
   return newUser;
