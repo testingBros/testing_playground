66a63cfdf44c0b40eb2e279baf31b66594fbea8f
Jay Steigner <ioctosteigner@gmail.com>
2021-02-10 00:07:33 -0600

    (fix) made test post request modular

---
+++ test/test_api_requests.spec.js
@@ -0,0 +1,23 @@
+const expect = require("chai").expect,
+{ random, name } = require("faker"),
+{ userCreation } = require("../userCreation.js"),
+chai = require('chai'),
+chaiHttp = require('chai-http'),
+server = 'http://localhost:3000';
+
+chai.use(chaiHttp);
+
+const mockPostRequest = async ({ userName, userHeight, userAge }) => {
+  return await chai.request(server)
+    .post('/api/')
+    .send({
+      'name': userName,
+      'height': userHeight,
+      'age': userAge
+  });
+}
+
+module.exports = {
+  mockPostRequest,
+}
+
