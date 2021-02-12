96011b9c504faf8352dc8186bb9501f3e0108bb9
xoweekday <xoweekday@gmail.com>
2021-02-12 11:33:52 -0600

    (add) a new error handling test checking if a user is in the database, also changed the status code that is sent back to the client when a user is not found.

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,15 @@
 const { userCreation, userRetrieval } = require("../db/dbHelpers/utils");
-const express = require('express');
+const express = require("express");
 const apiRouter = express.Router();
 
 apiRouter.post("/", async (req, res) => {
   try {
-    const { name, height, age} = req.body;
+    const { name, height, age } = req.body;
     const newUser = await userCreation(name, age, height);
     if (!newUser) throw "You have entered the wrong data type";
     res.status(201).send(newUser);
-  } catch(err) {
-      if (err) res.status(400).send(err);
+  } catch (err) {
+    if (err) res.status(400).send(err);
   }
 });
 
@@ -17,13 +17,13 @@ apiRouter.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const user = await userRetrieval(id);
-    if (!user) throw "user does not exist";
+    if (!user) throw "This user does not exist.";
     res.send(user);
-  } catch(err) {
-    if (err) res.status(400).send(err);
+  } catch (err) {
+    if (err) res.status(404).send(err);
   }
 });
 
 module.exports = {
   apiRouter,
-}
+};
