d320cfec4d5bef505e106e499b59c35ae4963a14
xoweekday <xoweekday@gmail.com>
2021-02-12 10:41:48 -0600

    (refactored & cleanedup) user_create.spec.js

--- test/test_api_requests.spec.js
+++ test/test_api_requests.spec.js
@@ -1,12 +1,10 @@
-const { random, name } = require("faker"),
-  { userCreation } = require("../db/dbHelpers/userCreation.js"),
-  chai = require("chai"),
+const chai = require("chai"),
   chaiHttp = require("chai-http"),
   server = "http://localhost:3000";
 
 chai.use(chaiHttp);
 
-const mockPostRequest = async ({ userName, userHeight, userAge }) => {
+const mockPostRequest = async ({ userName, userHeight, userAge } = {}) => {
   return await chai.request(server).post("/api/").send({
     name: userName,
     height: userHeight,
--- test/user_create.spec.js
+++ test/user_create.spec.js
@@ -14,13 +14,14 @@ before(async () => {
 });
 
 describe("creating a new newUser", () => {
-  // refactor to use mock post request
-
-  // context("without arguments", () => {
-  //   it("should log please include name, age, height", () => {
-  //     expect(userCreation()).to.equal(false);
-  //   });
-  // });
+  context("without arguments", () => {
+    it("should log please include name, age, height", async () => {
+      const {
+        error: { text: errorMessage },
+      } = await mockPostRequest();
+      expect(errorMessage).to.equal("You have entered the wrong data type");
+    });
+  });
 
   context("with arguments", () => {
     it("newUser's age should be an integer", () => {
@@ -42,10 +43,10 @@ describe("creating a new newUser", () => {
 });
 
 describe("when inserting a new newUser", () => {
-  it("201 status code should be received", async () =>
+  it("201 status code should be received", () =>
     expect(mockHttpResponse.statusCode).to.equal(201));
 
-  it("preDatabaseInserted user properties equals postDatabaseInserted user properties", async () => {
+  it("preDatabaseInserted user properties equals postDatabaseInserted user properties", () => {
     expect(userName).to.be.equal(newUser.name);
     expect(userHeight).to.be.equal(newUser.height);
     expect(userAge).to.be.equal(newUser.age);
@@ -56,19 +57,24 @@ describe("when inserting a new newUser", () => {
     falseUserAge,
     falseUserHeight,
     falseMockHttpResponse;
+
   before(async () => {
     falseUserName = random.number();
     falseUserAge = name.firstName();
     falseUserHeight = random.number();
-    falseNewUser = await mockPostRequest(userName, userAge, userHeight);
+    falseNewUser = await mockPostRequest(
+      falseUserName,
+      falseUserAge,
+      falseUserHeight
+    );
     falseMockHttpResponse = falseNewUser.res;
   });
 
   describe("a user is created with the wrong data type", () => {
-    it("400 status code should be received", async () =>
+    it("400 status code should be received", () =>
       expect(falseMockHttpResponse.statusCode).to.be.equal(400));
 
-    it("'You have entered the wrong data type' should be received", async () =>
+    it("'You have entered the wrong data type' should be received", () =>
       expect(falseMockHttpResponse.text).to.be.equal(
         "You have entered the wrong data type"
       ));
