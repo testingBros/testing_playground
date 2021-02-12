bb3bd382c7e0f7ed87689bd93b75f76535a55832
xoweekday <xoweekday@gmail.com>
2021-02-11 21:16:04 -0600

    (refactor) reduced the amount of mock http requests that were being made in the user_create.spec.js file

--- db/dbHelpers/userRetrieval.js
+++ db/dbHelpers/userRetrieval.js
@@ -2,7 +2,7 @@ const { db } = require("../index");
 
 const userRetrieval = async (currentUserId) => {
   const user = await db.query(
-    `SELECT * from users WHERE id = ${currentUserId}`
+    `SELECT * FROM users WHERE id = ${currentUserId}`
   );
   return user.rows[0];
 };
--- test/test_api_requests.spec.js
+++ test/test_api_requests.spec.js
@@ -1,22 +1,23 @@
 const { random, name } = require("faker"),
-{ userCreation } = require("../db/dbHelpers/userCreation.js"),
-chai = require('chai'),
-chaiHttp = require('chai-http'),
-server = 'http://localhost:3000';
+  { userCreation } = require("../db/dbHelpers/userCreation.js"),
+  chai = require("chai"),
+  chaiHttp = require("chai-http"),
+  server = "http://localhost:3000";
 
 chai.use(chaiHttp);
 
 const mockPostRequest = async ({ userName, userHeight, userAge }) => {
-  return await chai.request(server)
-    .post('/api/')
-    .send({
-      'name': userName,
-      'height': userHeight,
-      'age': userAge
+  return await chai.request(server).post("/api/").send({
+    name: userName,
+    height: userHeight,
+    age: userAge,
   });
-}
+};
+
+const mockGetRequest = async (currentUserId) =>
+  await chai.request(server).get(`/api/${currentUserId}`);
 
 module.exports = {
   mockPostRequest,
-}
-
+  mockGetRequest,
+};
