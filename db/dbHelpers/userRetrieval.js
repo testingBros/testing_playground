5a8f0f7acdaa6b3174e9f2eddd407b906cbdcaf7
xoweekday <xoweekday@gmail.com>
2021-02-11 19:46:30 -0600

    (add) functionality to retrieve users from the database based on user id

--- db/dbHelpers/userRetrieval.js
+++ db/dbHelpers/userRetrieval.js
@@ -1,9 +1,12 @@
-const { db } = require('../index');
+const { db } = require("../index");
 
-const userRetrieval = (id) => {
-  return id;
-}
+const userRetrieval = async (currentUserId) => {
+  const user = await db.query(
+    `SELECT * from users WHERE id = ${currentUserId}`
+  );
+  return user.rows[0];
+};
 
 module.exports = {
   userRetrieval,
-}
+};
--- test/user_read.spec.js
+++ test/user_read.spec.js
@@ -1,6 +1,6 @@
 const expect = require("chai").expect,
-{ mockPostRequest, mockGetRequest } = require("./test_api_requests.spec.js"),
-{ random, name } = require("faker");
+  { mockPostRequest, mockGetRequest } = require("./test_api_requests.spec.js"),
+  { random, name } = require("faker");
 
 describe("when retrieving a user", () => {
   let newUser, userName, userAge, userHeight;
@@ -8,19 +8,14 @@ describe("when retrieving a user", () => {
     userName = name.firstName();
     userAge = random.number();
     userHeight = `${random.number()}ft`;
-    newUser = await mockPostRequest({ userName, userHeight, userAge });  
-  });
-  
-  it("a 200 status code should be retrieved", () => {
-        
+    newUser = await mockPostRequest({ userName, userHeight, userAge });
   });
 
+  it("a 200 status code should be retrieved", () => {});
 });
 
-
 // when creating a new user
-  // then the same user should be accessible (GET)
+// then the same user should be accessible (GET)
 // finish making user retrieval function inside of userRetrieval.js
 // finish tests for user_read_spec.js (include things such as error handling, headers, cookies, ...)
 // discuss best practices for handling ids server/client-side
-
