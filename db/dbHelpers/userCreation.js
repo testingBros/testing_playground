27d8995803243875c668db66722e762d7460d2cf
xoweekday <xoweekday@gmail.com>
2021-02-26 20:48:48 -0600

    (refactor) userCreation to use a ternary

--- db/dbHelpers/userCreation.js
+++ db/dbHelpers/userCreation.js
@@ -1,18 +1,13 @@
 const { db } = require("../index");
-const userCreation = (username, age, height) => {
-  if (userPropertyValidation(username, age, height) === false) {
-    return false;
-  }
-
-  return dbInsertQuery({ username, age, height });
-};
+const userCreation = (username, age, height) =>
+  userPropertyValidation(username, age, height) === false
+    ? false
+    : dbInsertQuery({ username, age, height });
 
 const userPropertyValidation = (username, age, height) => {
   if (!username || !age || !height) {
     return false;
   }
-  // ask someone about best practices about this
-  // Is this function doing too much or should it be extrapolated out
   if (typeof username !== "string") return false;
   if (typeof height !== "string") return false;
 };
